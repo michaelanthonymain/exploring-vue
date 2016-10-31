c8c36e1cf9faa764942cc9f3e0bf667ed1d71516
Michael Main <michaelanthonymain@gmail.com>
2016-10-31 17:19:22 -0500

    moving javascript out of script tag

---
+++ index.js
@@ -0,0 +1,37 @@
+Vue.component('todo-item', {
+    // The todo-item component now accepts a
+    // "prop", which is like a custom attribute.
+    // This prop is called todo.
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+});
+
+var app7 = new Vue({
+    el: '#app-7',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'another one' }
+        ]
+    }
+});
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+});
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!'
+    }
+});
